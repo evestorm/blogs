61d8473bd226574b3b35e3ac940e2798fda8ce16
evelance <1572534835@qq.com>
2023-05-30 18:41:59 +0800

    feat: 新博客

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,117 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/blogs/',
+  title: "Lance's blog",
+  description: "Front-end developer and iOS developer",
+  lang: 'zh-CN',
+  // head 标签内容追加
+  head: [
+    ['link', { rel: 'icon', href: '/images/kitten.jpg' }]
+  ],
+  // 使用 git commit 获取时间戳。此选项允许默认主题显示页面的上次更新时间；通过 themeConfig.lastUpdatedText 选项自定义文本
+  lastUpdated: true,
+  markdown: {
+    lineNumbers: true, // 开启行号
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/images/kitten.jpg', // 站点左侧logo
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '前端',
+        activeMatch: '^/frontend/',
+        items: [
+          { text: 'HTML', link: '/frontend/html/基础/元素' },
+          { text: 'CSS', link: '/frontend/css/基础/CSS引入方式' },
+          // { text: 'JavaScript', link: '/frontend/js/JavaScript基础知识' },
+        ]
+      },
+      // { text: 'Examples', activeMatch: '^/other/', link: '/markdown-examples' }
+    ],
+
+    sidebar: {
+      "/frontend/html/": [
+        {
+          text: 'HTML',
+          items: [
+            {
+              text: '基础',
+              items: [
+                {
+                  text: '元素',
+                  link: '/frontend/html/基础/元素'
+                },
+                {
+                  text: '表格Table',
+                  link: '/frontend/html/基础/表格Table'
+                }
+              ]
+            },
+          ]
+        }
+      ],
+      "/frontend/css/": [
+        {
+          text: 'CSS',
+          items: [
+            {
+              text: '基础',
+              items: [
+                {
+                  text: 'CSS引入方式',
+                  link: '/frontend/css/基础/CSS引入方式'
+                },
+                {
+                  text: '盒模型',
+                  link: '/frontend/css/基础/盒模型'
+                }
+              ]
+            },
+          ]
+        }
+      ]
+    },
+
+    // sidebar: [
+    //   {
+    //     text: 'HTML',
+    //     items: [
+    //       {
+    //         text: '基础',
+    //         items: [
+    //           {
+    //             text: '元素',
+    //             link: '/frontend/html/基础/元素'
+    //           }
+    //         ]
+    //       },
+    //     ]
+    //   },
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/evestorm' }
+    ],
+
+    lastUpdatedText: '上次更新',
+    outlineTitle: '目录',
+
+    // 上一个和下一个链接上方显示的文本
+    docFooter: {
+      prev: '⬅️',
+      next: '➡️'
+    }
+  },
+  // 当设置为 true 时，VitePress 不会因死链接而导致构建失败。
+  ignoreDeadLinks: true
+})
---
+++ docs/frontend/html/基础/表格Table.md
@@ -0,0 +1,91 @@
+# 表格 Table
+
+## 属性
+
+- table `display: table`
+  - attr
+    - `border` 边框
+    - `cellpadding` 单元格内边距
+    - `cellspacing` 单元格边距
+  - `caption` 标题标签 `display: table-caption`
+  - `tr` - table row 表格行标签 `display: table-row`
+    - `th` - table header cell 表头标签 `display: table-cell`
+    - `td` - table data cell 单元格标签 `display: table-cell`
+      - `rowspan` - 合并n行
+      - `colspan` - 合并n列
+      - align - left | right | center 对齐方式
+  - `thead` - table head 表格页眉标签 `display: table-header-group`
+  - `tfoot` - table foot 表格页尾标签 `display: table-footer-group`
+  - `tbody` - table body 表格主体标签 `display: table-row-group`
+
+## 🌈 注意点
+
+- `thead、tfoot、tbody` 要写的意义：
+  - 不写，如果数据多，整个表格会等数据完全加载完后才显示；如果都写了，页眉和页尾则不用等数据完全加载完就会先展示出来
+- 加载顺序：`thead -> tfoot -> tbody`
+  - 就算把 `tfoot` 写在 `thead` 前面，也不会影响它们的显示顺序，`thead` 还是会在 `tfoot` 上面显示
+
+## 示例
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <table border="1" cellpadding="10" cellspacing="10">
+    <caption>用户</caption>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>姓名</th>
+        <th>电话</th>
+        <th>年龄</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Lance</td>
+        <td>18722212223</td>
+        <td>26</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>GC</td>
+        <td>18677721122</td>
+        <td>30</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Jerry</td>
+        <!-- 合并n列 -->
+        <td colspan="2">colspan合并2列</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>QB</td>
+        <td>17699921242</td>
+        <td rowspan="2">rowspan合并2行</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>Sherry</td>
+        <td>18677721122</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="4" align="right">合并整行</td>
+      </tr>
+    </tfoot>
+  </table>
+</body>
+</html>
+```
+
+![Table Demo](/images/表格Table/table-demo.png)
